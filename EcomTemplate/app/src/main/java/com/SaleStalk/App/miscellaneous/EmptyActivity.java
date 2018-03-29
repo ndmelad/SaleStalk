cbe8f36fdf48bc6c56abba7d96f81734eeef0095
Ndmelad <natsky27@gmail.com>
2018-03-29 18:14:52 +0800

    reworked UI

---
+++ EcomTemplate/app/src/main/java/com/SaleStalk/App/miscellaneous/EmptyActivity.java
@@ -0,0 +1,14 @@
+package com.SaleStalk.App.miscellaneous;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.SaleStalk.App.R;
+
+public class EmptyActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_empty);
+    }
+}
---
+++ EcomTemplate/app/src/main/java/com/SaleStalk/App/startup/LoginActivity.java
@@ -0,0 +1,80 @@
+package com.SaleStalk.App.startup;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.SaleStalk.App.startup.MainActivity;
+import com.SaleStalk.App.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    SQLiteDatabase db;
+    SQLiteOpenHelper openHelper;
+    Cursor cursor;
+    Button _btnLogin, _btnReg;
+    EditText _txtEmail, _txtPass;
+    // User Session Manager Class
+    UserSessionManager session;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        // User Session Manager
+        session = new UserSessionManager(getApplicationContext());
+
+        _txtEmail=(EditText)findViewById(R.id.txtEmail);
+        _txtPass=(EditText)findViewById(R.id.txtPass);
+        _btnLogin=(Button)findViewById(R.id.btnLogin);
+        _btnReg=(Button)findViewById(R.id.btnReg);
+        openHelper=new DatabaseHelper(this);
+        db = openHelper.getReadableDatabase();
+
+
+        _btnReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+        _btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = _txtEmail.getText().toString();
+                String pass = _txtPass.getText().toString();
+
+                cursor = db.rawQuery("SELECT *FROM " + DatabaseHelper.TABLE_NAME + " WHERE " + DatabaseHelper.COL_5 + "=? AND " + DatabaseHelper.COL_4 + "=?", new String[]{email, pass});
+                if (cursor != null) {
+                    if (cursor.getCount() > 0) {
+                        Toast.makeText(getApplicationContext(), "Login Success", Toast.LENGTH_SHORT).show();
+
+                        session.createUserLoginSession("change to user name",
+                                email);
+
+                        // Starting MainActivity
+                        Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+                        // Add new Flag to start new Activity
+                        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(i);
+
+                        finish();
+
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Login error", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+    }
+}
